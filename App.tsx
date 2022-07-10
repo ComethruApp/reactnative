1be9731eea3c72d38b068771270ad3c156812f15
ErikBoesen <me@erikboesen.com>
2022-07-09 22:01:30 -0700

    Add initial app content

--- App.tsx
+++ App.tsx
@@ -1,18 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Auth from './pages/Auth';
+import { Colors } from './colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Hello</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        width: '100%',
+        height: '100%',
+        backgroundColor: Colors.background,
+        color: Colors.foreground,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: Colors.foreground,
+    },
 });
