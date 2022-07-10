ffb4dc34a71e96283c5b0d04a955718b2ec72920
ErikBoesen <me@erikboesen.com>
2022-07-09 22:03:43 -0700

    Add colors constant file

---
+++ colors.ts
@@ -0,0 +1,62 @@
+export const Colors = {
+  foreground: '#ffffff',
+
+  background: '#15202b',
+  backgroundShade: '#121c26',
+  backgroundTint: '#2c3640',
+
+  primary: '#007ef1',
+  primaryContrast: '#ffffff',
+  primaryShade: '#0071d8',
+  primaryTint: '#198af2',
+
+  secondary: '#40d167',
+  secondaryContrast: '#ffffff',
+  secondaryShade: '#39bc5c',
+  secondaryTint: '#66da85',
+
+  tertiary: '#f46036',
+  tertiaryContrast: '#ffffff',
+  tertiaryShade: '#db5630',
+  tertiaryTint: '#f56f4a',
+
+  success: '#10dc60',
+  successContrast: '#ffffff',
+  successShade: '#0ec254',
+  successTint: '#28e070',
+
+  warning: '#ffce00',
+  warningContrast: '#ffffff',
+  warningShade: '#e0b500',
+  warningTint: '#ffd31a',
+
+  danger: '#f04141',
+  dangerContrast: '#ffffff',
+  dangerShade: '#d33939',
+  dangerTint: '#f25454',
+
+  dark: '#2c3640',
+  darkContrast: '#ffffff',
+  darkShade: '#273039',
+  darkTint: '#3c444c',
+
+  medium: '#555D66',
+  mediumContrast: '#ffffff',
+  mediumShade: '#4c535b',
+  mediumTint: '#666d75',
+
+  light: '#f4f5f8',
+  lightContrast: '#000000',
+  lightShade: '#d7d8da',
+  lightTint: '#f5f6f9',
+
+  facebook: '#4267B2',
+  facebookContrast: '#ffffff',
+  facebookShade: '#3b5ca0',
+  facebookTint: '#5476b9',
+
+  venmo: '#3396CD',
+  venmoContrast: '#ffffff',
+  venmoShade: '#2d87b8',
+  venmoTint: '#47a0d2',
+};
