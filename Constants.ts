303631f6a63e3f61314107471354671e1d23bbab
ErikBoesen <me@erikboesen.com>
2022-07-16 12:01:28 -0700

    Add Constants file

---
+++ Constants.ts
@@ -0,0 +1,8 @@
+export const Constants: {
+    HOST: 'https://api.comethru.io',
+//    HOST: 'http://localhost:5000',
+
+    ONESIGNAL_APPID: 'PLACEHOLDER',
+    GOOGLE_PROJECT_NUMBER: 'PLACEHOLDER',
+};
+
