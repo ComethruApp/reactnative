e70ffa00df0a8715e7481323959652ceb05d029e
ErikBoesen <me@erikboesen.com>
2022-07-15 19:42:17 -0700

    Add Link component

---
+++ components/Link.tsx
@@ -0,0 +1,29 @@
+import React, { PropTypes, Component } from 'react';
+import { StyleSheet, Linking } from 'react-native';
+import { Colors } from '../colors';
+import AppText from './AppText';
+
+export default class AppTextInput extends Component {
+    getProps() {
+        const { style, url, ...props } = this.props;
+        return props;
+    }
+
+    render() {
+        return (
+            <AppText
+                style={[this.props.style, styles.component]}
+                onPress={() => Linking.openURL(this.props.url)}
+                {...this.getProps()}>
+                {this.props.children}
+            </AppText>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    component: {
+        textDecoration: 'underline',
+    },
+});
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,6 @@
 export AppText from './AppText';
 export AppTextInput from './AppTextInput';
 export AppButton from './AppButton';
+export Link from './Link';
+
+export Logo from './Logo';
