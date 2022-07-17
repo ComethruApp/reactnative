03895aebde9b882f6401a3311cc6bf20c9737717
ErikBoesen <me@erikboesen.com>
2022-07-16 17:35:53 -0700

    Add starting HTTPService

---
+++ services/HTTPService.ts
@@ -0,0 +1,30 @@
+import Constants from '../Constants';
+
+class HTTPService {
+
+    get(path: string) {
+        //return fetch(token => this.httpClient.get(this.ROOT + path, this.ops(token)));
+        return fetch(this.HOST+ '/' + path, {
+            method: 'GET',
+        });
+    }
+    post(path: string, data: any) {
+        //return fetch(token => this.httpClient.post(this.ROOT + path, data, this.ops(token)));
+        return fetch(this.HOST + '/' + path, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+    }
+    delete(path: string) {
+        //return fetch(token => this.httpClient.delete(this.ROOT + path, this.ops(token)));
+    }
+    put(path: string, data: any) {
+        //return fetch(token => this.httpClient.put(this.ROOT + path, data, this.ops(token)));
+    }
+}
+
+export default httpService = HTTPService();
