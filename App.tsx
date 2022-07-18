4c68213b840f75a7fbaa27d6605ca132ca65d1c2
ErikBoesen <me@erikboesen.com>
2022-07-17 19:43:51 -0700

    Start configuring main router

--- App.tsx
+++ App.tsx
@@ -3,15 +3,27 @@ import { NativeRouter, Routes, Route } from 'react-router-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { Colors } from './colors';
+import { authService } from './services';
 import { Auth } from './pages';
 
 export default function App() {
+    /*
+    const [fontsLoaded] = Font.useFonts({
+        'JosefinSans-Regular': require('./assets/fonts/JosefinSans-Regular.ttf'),
+    });
+    */
+
+    const { path } = this.props.match;
+
     return (
         <SafeAreaProvider>
             <View style={styles.container}>
                 <NativeRouter>
                     <Routes>
-                        <Route exact path='/' element={<Auth />} />
+                        <Redirect from='/' to={authService.isLoggedIn() ? '/tabs' : '/auth'} />
+
+                        <Route path={`${path}/auth`} element={<Auth />} />
+                        <Route path={`${path}/tabs`} element={<Tabs />} />
                     </Routes>
                 </NativeRouter>
             </View>
