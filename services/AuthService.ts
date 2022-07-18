2099c4076e39f8d44aac881e12b8a4c1949463ab
ErikBoesen <me@erikboesen.com>
2022-07-17 19:44:58 -0700

    Refactor AuthService

--- services/AuthService.ts
+++ services/AuthService.ts
@@ -1,52 +1,52 @@
-import { useNavigate } from 'react-router-router';
+import { useNavigate } from 'react-router-native';
 
 import storage from './Storage';
 import httpService from './HTTPService';
 
 
 class AuthService {
-    /*
-    let navigate = useNavigate();
 
-    const ROOT: string = 'auth';
-    */
+    constructor() {
+        this.ROOT = 'auth';
+        //let navigate = useNavigate();
+    }
 
     register(user) {
-        return httpService.post(ROOT + '/register', {
+        return httpService.post(this.ROOT + '/register', {
             method: 'POST',
             body: JSON.stringify(user),
         });
-    }
+    };
 
     login(user) {
-        return httpService.post(ROOT + '/login', user)
+        return httpService.post(this.ROOT + '/login', user)
             .then((response) => {
                 if (response.user) {
                     storage.save({key: 'token', data: response.user.token});
                     //this.oneSignal.setExternalUserId(response.user.id.toString());
                 }
+            })
+            .catch((error) => {
+                console.log(error);
             });
-    }
+    };
 
     async logout() {
         await storage.remove({key: 'token'});
         // Remove navigation stack so next user to log in won't see this user's views
         //this.router.initialNavigation();
-        navigate('/auth');
-    }
+        //navigate('/auth');
+    };
 
     isLoggedIn() {
         // Check if user has logged in.
         // Note that this does not check if the token we have is actually valid.
-        return storage.load({key: 'token'})
-            .then(token => {
-                return token != null;
-            });
-    }
+        return (await storage.load({key: 'token'}) != null);
+    };
 
     resetPassword(payload) {
         return httpService.post(ROOT + '/reset_password_request', payload);
-    }
+    };
 }
 
 export default authService = new AuthService();
